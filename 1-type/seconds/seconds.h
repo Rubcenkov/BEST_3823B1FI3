7617057e802f7701dab88dcee98cef2a72a1464a
Rubcenkov <rubcenkovvlad977@gmail.com>
2023-10-24 18:48:33 +0300

    dfdf

--- 1-type/seconds/seconds.h
+++ 1-type/seconds/seconds.h
@@ -1,28 +1,33 @@
+#include <math.h>
 int get_years(int seconds) {
-    return 0; // Not implementation
+    int year;
+    year = (seconds / (60 * 60 * 24 * 31 * 12)) + 1970;
+    return year;
 }
-
 int get_month(int seconds) {
-    return 0; // Not implementation
+    int month;
+    month = (seconds / (60 * 60 * 24 * 31)) % 12;
+    return month;
 }
-
 int get_day(int seconds) {
-    return 0; // Not implementation
+    int day;
+    day = (seconds / (60 * 60 * 24)) % 31;
+    return day;
 }
-
 int get_hours(int seconds) {
-    return 0; // Not implementation
+    int hours;
+    hours = (seconds % (60 * 60 * 24)) / 3600;
+    return hours;
 }
-
 int get_minutes(int seconds) {
-    return 0; // Not implementation
+    int min;
+    min = ((seconds % (60 * 60 * 24)) % (60*60)) / 60;
+    return min;
 }
-
 int get_seconds(int seconds) {
-    return 0; // Not implementation
+    seconds = ((seconds % (60 * 60 * 24)) % (60 * 60)) % 60;
+    return seconds;
 }
-
-
 int get_magical_value(int seconds) {
     int year = get_years(seconds);
     int month = get_month(seconds);
@@ -30,5 +35,5 @@ int get_magical_value(int seconds) {
     int hours = get_hours(seconds);
     int minutes = get_minutes(seconds);
     int second = get_seconds(seconds);
-    return 0; // Not implementation
-}
+    int s = ((year + month) / (1 + day + second)) * ((year + month) / (1 + day + second)) + (minutes + (minutes / (1 + hours))) / (1 + hours);
+    return s;
