a2a5ec5d78f9cdb1e3ac636ab924cde81809fa47
Rubcenkov <rubcenkovvlad977@gmail.com>
2023-10-24 18:43:10 +0300

    dfdf

--- 1-type/clock/clock.h
+++ 1-type/clock/clock.h
@@ -1,3 +1,14 @@
-int get_time(long long angle) {
-    return 0; // Not implementation
+int get_time(long long angl) {
+    int min, ch, sec;
+    if (angl<360){
+        return angl/6;}
+    else if(angl>=360 && angl<3600){
+        min = angl/360;
+        sec = (angl-(min*360))/6;
+        return min*100+sec;}
+    else{
+        chas = angl/3600;
+        min = (angl-(ch*3600))/360;
+        sec = (angl-(ch*3600)-(min*360))/6;
+        return ((ch*10)+min)*100+sec;}
 }
