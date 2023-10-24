7e57fd276cf4bf8bbfe273d1dd1f702988fed0c8
Rubcenkov <rubcenkovvlad977@gmail.com>
2023-10-24 18:44:43 +0300

    dfdf

--- 1-type/deadline/deadline.h
+++ 1-type/deadline/deadline.h
@@ -1,3 +1,14 @@
-int get_score(short score, int start, int stop, int now) {
-    return 0; // Not implementation
+int get_score(short sc, int start, int stop, int now){
+    if (now <= start){
+        return sc;}
+    if (now >= stop){
+        return (sc+1)/2;}
+    if ((now > start) && (now < stop)){
+        float res,score_half;
+        int ans, res_int;
+        score_half = sc/2;
+        res = score_half / (stop - start) * (now - start) + 0.1;
+        res_int = res; 
+        ans = sc - res_int;
+        return ans;}  
 }
