d7b3e40a216130dbdb196c2917358ffbc6ab7994
Rubcenkov <rubcenkovvlad977@gmail.com>
2023-10-27 17:45:01 +0300

    dff

--- 3-loop/bin_patterns/find_bin_pattern.h
+++ 3-loop/bin_patterns/find_bin_pattern.h
@@ -1,5 +1,14 @@
 #include <math.h>
-
-char find_bin_pattern(int number){
-    return 100;
+char find_bin_pattern(int numb){
+    int m[100] = {}, t = 0, c = 0;
+    while (numb>0){
+        m[t] = numb%2;
+        numb = numb/2;
+        t+=1;
+    }
+    for (int i = 0; i<97; i++){
+        if (m[i] == 1 && m[i+1] == 0 && m[i+2] == 1){
+            c+=1;}
+    }
+    return c;
 }
