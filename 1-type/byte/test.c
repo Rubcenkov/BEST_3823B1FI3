4208f79ffec6f1bc5e51720b5c20648b0e20d3ac
Rubcenkov <rubcenkovvlad977@gmail.com>
2023-10-27 17:31:53 +0300

    dfdf

--- 1-type/byte/test.c
+++ 1-type/byte/test.c
@@ -1,17 +1,15 @@
-unsigned char max_byte(unsigned short number) {
-    unsigned char maxByte = 0;
+#include "byte.h"
+#include "acutest.h"
 
-    // Разделяем переменную на два байта
-    unsigned char byte1 = number & 0xFF;
-    unsigned char byte2 = (number >> 8) & 0xFF;
 
-    // Сравниваем байты и находим максимальный
-    if (byte1 > maxByte) {
-        maxByte = byte1;
-    }
-    if (byte2 > maxByte) {
-        maxByte = byte2;
-    }
-
-    return maxByte;
+void test_byte() {
+    TEST_ASSERT(max_byte(65535) == 255);
+    TEST_ASSERT(max_byte(10000) == 39);
+    TEST_ASSERT(max_byte(20) == 20);
+    TEST_ASSERT(max_byte(32832) == 128);
 }
+
+TEST_LIST = {
+    {"Byte test", test_byte},
+    {NULL, NULL}
+};
---
+++ 1-type/decomposition/decomposition.h
@@ -0,0 +1,11 @@
+int reverse(int number){
+    return 0;
+}
+
+double decomposition(int number){
+    return 0;
+}
+
+int append(int number, int start, int end){
+    return 0;
+}
